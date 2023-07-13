42571a7b411ca8100005294c99940600b16c939c
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-07-13 12:11:07 +0700

    update: order model

--- backend-go/models/order.go
+++ backend-go/models/order.go
@@ -6,11 +6,11 @@ type OrderStatus struct {
 }
 
 type Order struct {
-	IDUser      string    `json:"idUser"`
-	IDStore     string    `json:"idStore"`
-	IDPromo     string    `json:"idPromo"`
-	DateOrder   time.Time `json:"dateOrder"`
-	Status      string    `json:"status"`
-	OrderedFood []Food    `json:"orderedFood"`
-	PickupTime  time.Time `json:"pickupTime"`
+	IDUser       string    `json:"idUser"`
+	IDStore      string    `json:"idStore"`
+	IDPromo      string    `json:"idPromo"`
+	DateOrder    time.Time `json:"dateOrder"`
+	Status       string    `json:"status"`
+	OrderedFoods []Food    `json:"orderedFoods"`
+	PickupTime   time.Time `json:"pickupTime"`
 }
