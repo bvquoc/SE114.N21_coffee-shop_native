dbc9231962611084bd5acaa217e2d6a4d8e4bf01
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2023-07-13 18:13:52 +0700

    dev: topping fragment

---
+++ coffee_shop_staff_admin/app/src/main/java/com/example/coffee_shop_staff_admin/models/Topping.java
@@ -0,0 +1,46 @@
+package com.example.coffee_shop_staff_admin.models;
+
+import android.net.Uri;
+
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.Map;
+
+public class Topping {
+    private String id;
+    private String name;
+    private double price;
+    private String image;
+
+    public Topping(String id, String name, double price, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+    public static Topping fromFireBase(QueryDocumentSnapshot doc)
+    {
+        String id = doc.getId();
+        Map<String, Object> map = doc.getData();
+        String name = map.get("name").toString();
+        double price = ((Number) map.get("price")).doubleValue();
+        String image = map.get("image").toString();
+        return new Topping(id, name, price, image);
+    }
+}
---
+++ coffee_shop_staff_admin/app/src/main/java/com/example/coffee_shop_staff_admin/utils/interfaces/OnToppingAdminClickListener.java
@@ -0,0 +1,7 @@
+package com.example.coffee_shop_staff_admin.utils.interfaces;
+
+import com.example.coffee_shop_staff_admin.models.Topping;
+
+public interface OnToppingAdminClickListener {
+    void onToppingAdminClickListener(String toppingId);
+}
