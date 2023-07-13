2ff927ad31e583a2c9ad13ca982621989a8a0a36
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-07-14 00:32:06 +0700

    dev: api order pickup (not optimized)

--- backend-go/constants/order.go
+++ backend-go/constants/order.go
@@ -2,3 +2,8 @@ package constants
 
 const ORDER_PROCESSING = "Đang xử lí"
 const ORDER_DELIVERING = "Đang giao"
+const ORDER_DELIVERED = "Đã giao"
+const ORDER_DELIVERY_FAILED = "Giao hàng thất bại"
+const ORDER_READY = "Đã chuẩn bị"
+const ORDER_COMPLETE = "Hoàn thành"
+const ORDER_CANCELLED = "Đã hủy"
--- backend-go/services/order.go
+++ backend-go/services/order.go
@@ -1,12 +1,14 @@
 package services
 
 import (
+	"coffee_shop_backend/constants"
 	app_context "coffee_shop_backend/context"
 	"coffee_shop_backend/models"
 	"encoding/json"
 	"fmt"
 	"math"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -19,13 +21,15 @@ func OrderCreate(c *gin.Context) {
 		return
 	}
 
-	user, err := app_context.AppFirestoreClient.GetDocumentMap("users", newOrder.IDUser)
+	_, err := app_context.AppFirestoreClient.GetDocumentMap("users", newOrder.IDUser)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Permission denied! User is not created."})
 		return
 	}
+	// fmt.Println(user)
 
-	fmt.Println(user)
+	newOrder.Status = constants.ORDER_PROCESSING
+	calcPrice(&newOrder)
 
 	dataBytes, err := json.Marshal(newOrder)
 	if err != nil {
@@ -37,21 +41,22 @@ func OrderCreate(c *gin.Context) {
 	var data map[string]interface{}
 	json.Unmarshal(dataBytes, &data)
 
-	// ordersId, err := app_context.AppFirestoreClient.CreateDocument("beorders", data)
-	// if err != nil {
-	// 	fmt.Println("Error create firestore document:", err)
-	// 	c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
-	// 	return
-	// }
+	// done := make(chan struct{})
+	// go func() {
+	// 	calcPrice(&newOrder)
+	// 	done <- struct{}{}
+	// }()
+	// <-done
 
-	// newOrder.PriceDiscount = 0
-	// newOrder.PriceTotal = 100
-	// newOrder.PriceProducts = 100
-	calcPrice(&newOrder)
+	ordersId, err := app_context.AppFirestoreClient.CreateDocument("beorders", data)
+	if err != nil {
+		fmt.Println("Error create firestore document:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
+		return
+	}
 
-	c.JSON(http.StatusCreated, newOrder)
-
-	// c.JSON(http.StatusCreated, map[string]interface{}{"orderID": ordersId})
+	// c.JSON(http.StatusCreated, newOrder)
+	c.JSON(http.StatusCreated, map[string]interface{}{"orderID": ordersId})
 }
 
 func calcPrice(ord *models.Order) {
@@ -77,24 +82,52 @@ func calcPrice(ord *models.Order) {
 		ord.PriceProducts += foodPrice * int64(sl)
 
 		// topping
-		// for _, t := range v.Toppings {
-		// 	fmt.Println(t)
-		// 	respTopping, _ := app_context.AppFirestoreClient.GetDocumentMap("Topping", t)
-		// 	fmt.Println(respTopping["price"])
-		// 	toppingPrice := respTopping["price"]
+		for _, t := range v.Toppings {
+			// fmt.Println(t)
+			respTopping, _ := app_context.AppFirestoreClient.GetDocumentMap("Topping", t)
+			fmt.Println(respTopping["price"])
+			// toppingPrice := respTopping["price"]
+			toppingPrice, err := strconv.ParseInt(fmt.Sprint(respTopping["price"]), 10, 64)
+			if err != nil {
+				fmt.Println("Failed to convert the 'price' field to int64")
+			} else {
+				ord.PriceProducts += toppingPrice * int64(sl)
+			}
 
-		// 	toppingPrice, ok := respTopping["price"].(int64)
-		// 	if !ok {
-		// 		fmt.Println("Failed to convert the 'price' field to int64")
-		// 	} else {
-		// 		ord.PriceProducts += toppingPrice.(int64) * int64(sl)
-		// 	}
-
-		// }
+		}
 	}
 
 	if len(ord.IDPromo) > 0 {
-		ord.PriceDiscount = 1
+		// ord.PriceDiscount = 1
+		respPromo, _ := app_context.AppFirestoreClient.GetDocumentMap("Promo", ord.IDPromo)
+		// fmt.Print(respPromo)
+
+		fmt.Println(respPromo["stores"])
+		// fmt.Println(reflect.TypeOf(respPromo["stores"]))
+
+		canUse := false
+		for _, v := range respPromo["stores"].([]interface{}) {
+			if s, ok := v.(string); ok {
+				// stores = append(stores, s)
+				if s == ord.IDStore {
+					canUse = true
+					break
+				}
+			}
+		}
+
+		if canUse {
+			canUse = false
+			atLeastPrice := respPromo["minPrice"].(int64)
+			maxDiscount := respPromo["maxPrice"].(int64)
+
+			fmt.Printf("%v %v\n", atLeastPrice, maxDiscount)
+
+			if ord.PriceProducts >= atLeastPrice {
+				ord.PriceDiscount = int64(math.Min(float64(ord.PriceProducts)*respPromo["percent"].(float64), float64(maxDiscount)))
+			}
+		}
+
 	}
 
 	ord.PriceTotal = int64(math.Max(0.0, float64(ord.PriceProducts-ord.PriceDiscount+ord.DeliveryCost)))
