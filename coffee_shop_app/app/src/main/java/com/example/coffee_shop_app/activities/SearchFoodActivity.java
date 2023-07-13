8d9fd4409aca11523590ab343be857839d475e70
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2023-07-13 16:57:13 +0700

    dev: search food feature

--- coffee_shop_app/app/src/main/java/com/example/coffee_shop_app/activities/SearchFoodActivity.java
+++ coffee_shop_app/app/src/main/java/com/example/coffee_shop_app/activities/SearchFoodActivity.java
@@ -1,10 +1,15 @@
 package com.example.coffee_shop_app.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
@@ -17,43 +22,55 @@ import android.widget.TextView;
 
 import com.example.coffee_shop_app.Data;
 import com.example.coffee_shop_app.R;
+import com.example.coffee_shop_app.activities.store.StoreDetailActivity;
+import com.example.coffee_shop_app.adapters.StoreAdapter;
+import com.example.coffee_shop_app.databinding.ActivitySearchFoodBinding;
+import com.example.coffee_shop_app.databinding.ActivityStoreSearchBinding;
+import com.example.coffee_shop_app.models.Store;
+import com.example.coffee_shop_app.utils.interfaces.OnProductClickListener;
+import com.example.coffee_shop_app.utils.interfaces.OnStoreClickListener;
 import com.example.coffee_shop_app.utils.styles.RecyclerViewGapDecoration;
 import com.example.coffee_shop_app.adapters.ProductAdapter;
 import com.example.coffee_shop_app.models.Product;
+import com.example.coffee_shop_app.viewmodels.ProductSearchViewModel;
+import com.example.coffee_shop_app.viewmodels.StoreSearchViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchFoodActivity extends AppCompatActivity {
-    private Toolbar toolbar;
-    private EditText searchEditText;
-    private TextView clearButton;
-    private TextView introduceTextView;
-    private RecyclerView searchProductRecycleView;
-    private ProductAdapter searchProductAdapter;
-
-    private Handler handler = new Handler();
+    private static final String TAG = "SearchFoodActivity";
+    private ActivitySearchFoodBinding activitySearchFoodBinding;
+    private ProductAdapter productAdapter;
+    private final Handler handler = new Handler();
     private Runnable searchRunnable;
-
+    private final int MILISECOND_DELAY_SEARCH  = 300;
+    private OnProductClickListener listener;
+    private final ActivityResultLauncher<Intent> activitySeeProductDetailResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        Intent intent = new Intent();
+                        intent.putExtra("productId", data.getStringExtra("productId"));
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                } else {
+                    //User do nothing
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_food);
 
-        toolbar = findViewById(R.id.my_toolbar);
-        searchEditText = findViewById(R.id.search_edit_text);
-        clearButton = findViewById(R.id.clear_button);
-        searchProductRecycleView = findViewById(R.id.search_product_recyclerview);
-        introduceTextView = findViewById(R.id.introduce_text_view);
+        activitySearchFoodBinding = DataBindingUtil.setContentView(this, R.layout.activity_search_food);
 
-        float dp = TypedValue
-                .applyDimension(
-                        TypedValue.COMPLEX_UNIT_DIP,
-                        1,
-                        getResources().getDisplayMetrics()
-                );
-
-        toolbar.setTitle("Search");
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Tìm kiếm cửa hàng");
         setSupportActionBar(toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,68 +78,66 @@ public class SearchFoodActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
-        List<Product> products = new ArrayList<Product>(Data.instance.products);
-        for(int i = Data.instance.favoriteProducts.size() - 1; i >=0; i--)
-        {
-            Product favoriteProduct = Data.instance.favoriteProducts.get(i);
-            for(int j = 0; j < products.size() - 1; j++)
-            {
-                Product product = products.get(j);
-                Log.e(product.getId(), favoriteProduct.getId());
-                if (product.getId().equals(favoriteProduct.getId())) {
-                    products.remove(j);
-                    products.add(0,favoriteProduct);
-                    break;
-                }
+        listener = new OnProductClickListener() {
+            @Override
+            public void onProductClick(String productId) {
+                Intent intent = new Intent(getApplicationContext(), ProductDetailActivity.class);
+                intent.putExtra("productId", productId);
+                activitySeeProductDetailResultLauncher.launch(intent);
             }
-        }
+        };
 
-        searchProductRecycleView.setLayoutManager(new LinearLayoutManager(this));
-        searchProductRecycleView.addItemDecoration(new RecyclerViewGapDecoration((int) (8*dp)));
-        searchProductAdapter = new ProductAdapter(products, false, true);
-        searchProductRecycleView.setAdapter(searchProductAdapter);
-
-        searchEditText.addTextChangedListener(new TextWatcher() {
+        init();
+    }
+    private void init() {
+        ProductSearchViewModel productSearchViewModel = new ProductSearchViewModel();
+        productAdapter = new ProductAdapter(new ArrayList<Product>(), true);
+        productAdapter.setOnProductClickListener(listener);
+        activitySearchFoodBinding.searchProductRecyclerview.setAdapter(productAdapter);
+        productSearchViewModel.getLiveProductData().observe(this, productList -> {
+            if (productList == null) {
+                productSearchViewModel.getIsLoading().setValue(true);
+                return;
+            }
+            productAdapter.changeDataSet(productList);
+            productSearchViewModel.getIsLoading().setValue(false);
+        });
+        productSearchViewModel.getIsLoading().observe(this, isLoading -> {
+            if (isLoading) {
+                activitySearchFoodBinding.main.setVisibility(View.GONE);
+                activitySearchFoodBinding.shimmerLayout.setVisibility(View.VISIBLE);
+                activitySearchFoodBinding.shimmerLayout.startShimmer();
+            } else {
+                activitySearchFoodBinding.main.setVisibility(View.VISIBLE);
+                activitySearchFoodBinding.shimmerLayout.setVisibility(View.GONE);
+                activitySearchFoodBinding.shimmerLayout.stopShimmer();
+            }
+        });
+        activitySearchFoodBinding.setViewModel(productSearchViewModel);
+        activitySearchFoodBinding.searchProductRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        activitySearchFoodBinding.searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() > 0) {
-                    clearButton.setVisibility(View.VISIBLE);
-                } else {
-                    clearButton.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
                 handler.removeCallbacks(searchRunnable);
                 searchRunnable = new Runnable() {
                     @Override
                     public void run() {
-                        searchProductAdapter.getFilter().filter(s);
-                        if ( s!=null && s.length() > 0) {
-                            introduceTextView.setVisibility(View.GONE);
-                        } else {
-                            introduceTextView.setVisibility(View.VISIBLE);
-                        }
+                        productAdapter.getFilter().filter(s);
                     }
 
                 };
-                handler.postDelayed(searchRunnable, 300);
+                handler.postDelayed(searchRunnable, MILISECOND_DELAY_SEARCH);
             }
-        });
 
-        clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                searchEditText.setText("");
+            public void afterTextChanged(Editable s) {
+
             }
         });
-
-        searchEditText.setText("");
     }
 }
