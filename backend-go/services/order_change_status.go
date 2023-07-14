886cc67a804ebbf71000b519e7f2134dcd5b2166
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-07-14 16:00:34 +0700

    update: repsonse order status

--- backend-go/services/order_change_status.go
+++ backend-go/services/order_change_status.go
@@ -9,19 +9,18 @@ import (
 )
 
 func OrderChangeStatus(c *gin.Context) {
-	// c.JSON(http.StatusOK, gin.H{"orders": make([]int, 0)})
 	orderID := c.Param("orderId")
 
 	var requestBody struct {
 		Status string `json:"status"`
 	}
 	if err := c.ShouldBindJSON(&requestBody); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"orderId": orderID, "error": err.Error()})
 		return
 	}
 	if !constants.IsOrderStatus(requestBody.Status) {
 		if err := c.ShouldBindJSON(&requestBody); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order status"})
+			c.JSON(http.StatusBadRequest, gin.H{"orderId": orderID, "error": "Invalid order status"})
 			return
 		}
 	}
@@ -30,7 +29,7 @@ func OrderChangeStatus(c *gin.Context) {
 	data["status"] = requestBody.Status
 	err := app_context.AppFirestoreClient.UpdateDocument(constants.CLT_ORDER, orderID, data)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"orderId": orderID, "error": "Invalid order ID"})
 		return
 	}
 
