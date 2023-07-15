204bd329213fdea1b6b625103453460707230e13
kimthu09 <21521495@gm.uit.edu.vn>
2023-07-15 23:18:46 +0700

    dev: order screen functions

--- coffee_shop_app/app/src/main/java/com/example/coffee_shop_app/fragments/OrderDeliveryListFragment.java
+++ coffee_shop_app/app/src/main/java/com/example/coffee_shop_app/fragments/OrderDeliveryListFragment.java
@@ -5,25 +5,39 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.coffee_shop_app.R;
 import com.example.coffee_shop_app.adapters.DeliveryCardAdapter;
+import com.example.coffee_shop_app.adapters.PickupCardAdapter;
+import com.example.coffee_shop_app.models.Order;
+import com.example.coffee_shop_app.repository.OrderRepository;
+import com.example.coffee_shop_app.viewmodels.OrderViewModel;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderDeliveryListFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private DeliveryCardAdapter adapter;
-
+    private LinearLayout noOrderView;
+    private boolean isHistory;
+    private SwipeRefreshLayout refreshLayout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if (getArguments() != null) {
+            isHistory = getArguments().getBoolean("isHistory");
+        }
     }
 
     @Override
@@ -32,15 +46,78 @@ public class OrderDeliveryListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_order_delivery_list, container, false);
     }
-
+    public static OrderDeliveryListFragment newInstance(boolean isHistory) {
+        OrderDeliveryListFragment fragment = new OrderDeliveryListFragment();
+        Bundle args = new Bundle();
+        args.putBoolean("isHistory", isHistory);
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         recyclerView= view.findViewById(R.id.recyclerDeliveryCard);
-        adapter=new DeliveryCardAdapter(view.getContext());
-        recyclerView.setNestedScrollingEnabled(false);
-        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        recyclerView.setAdapter(adapter);
+        noOrderView=view.findViewById(R.id.noOrderView);
+        refreshLayout=view.findViewById(R.id.refreshLayout);
+        getOrder();
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                OrderRepository.getInstance().registerSnapshotListener();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
     }
+
+    private void getOrder(){
+        if(isHistory){
+            OrderViewModel.getInstance().getOrderDeliListMutableLiveData().observe(getViewLifecycleOwner(), new Observer<List<Order>>() {
+                @Override
+                public void onChanged(List<Order> orders) {
+                    List<Order> historyList=orders
+                            .stream()
+                            .filter(ord->ord.getStatus().equals(getString(R.string.statusDelivered))
+                                    || ord.getStatus().equals(getString(R.string.statusComplete))
+                                    ||ord.getStatus().equals(getString(R.string.statusCancelled))
+                                    ||ord.getStatus().equals(getString(R.string.statusDeliveryFailed)))
+                            .collect(Collectors.toList());
+                    adapter=new DeliveryCardAdapter(historyList);
+                    recyclerView.setNestedScrollingEnabled(false);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    recyclerView.setAdapter(adapter);
+
+                    if(historyList.size()==0 && !OrderViewModel.getInstance().isLoading()){
+                        noOrderView.setVisibility(View.VISIBLE);
+                    }else{
+                        noOrderView.setVisibility(View.GONE);
+                    }
+                }
+            });
+        } else{
+            OrderViewModel.getInstance().getOrderDeliListMutableLiveData().observe(getViewLifecycleOwner(), new Observer<List<Order>>() {
+                @Override
+                public void onChanged(List<Order> orders) {
+                    List<Order> normalList=orders
+                            .stream()
+                            .filter(ord-> !ord.getStatus().equals(getString(R.string.statusDelivered))
+                                    && !ord.getStatus().equals(getString(R.string.statusComplete))
+                                    && !ord.getStatus().equals(getString(R.string.statusCancelled))
+                                    && !ord.getStatus().equals(getString(R.string.statusDeliveryFailed)))
+                            .collect(Collectors.toList());
+                    adapter=new DeliveryCardAdapter(normalList);
+                    recyclerView.setNestedScrollingEnabled(false);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    recyclerView.setAdapter(adapter);
+                    if(normalList.size()==0 && !OrderViewModel.getInstance().isLoading()){
+                        noOrderView.setVisibility(View.VISIBLE);
+                    }else{
+                        noOrderView.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+    }
+
 }
