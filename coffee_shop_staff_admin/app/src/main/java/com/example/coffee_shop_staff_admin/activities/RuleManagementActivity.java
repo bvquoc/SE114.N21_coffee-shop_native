2e8eb8a3c100ca481835bd7d5d0f75c290cc6656
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-07-16 00:13:33 +0700

    dev: user role feature

---
+++ coffee_shop_staff_admin/app/src/main/java/com/example/coffee_shop_staff_admin/activities/RuleManagementActivity.java
@@ -0,0 +1,232 @@
+package com.example.coffee_shop_staff_admin.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.coffee_shop_staff_admin.R;
+import com.example.coffee_shop_staff_admin.databinding.ActivityRuleManagementBinding;
+import com.example.coffee_shop_staff_admin.models.User;
+import com.example.coffee_shop_staff_admin.repositories.UserRepository;
+import com.example.coffee_shop_staff_admin.viewmodels.RuleManagementViewModel;
+
+public class RuleManagementActivity extends AppCompatActivity {
+    private final String TAG = "RuleManagementActivity";
+    private ActivityRuleManagementBinding activityRuleManagementBinding;
+    private final RuleManagementViewModel ruleManagementViewModel = new RuleManagementViewModel();
+    private AsyncTask<Void, Void, Void> searchTask;
+
+    private AsyncTask<Void, Void, Void> updateTask;
+
+    private User selectedUser = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        activityRuleManagementBinding = DataBindingUtil.setContentView(this, R.layout.activity_rule_management);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Quyền truy cập");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        init();
+    }
+    private void init()
+    {
+        activityRuleManagementBinding.setViewModel(ruleManagementViewModel);
+
+        activityRuleManagementBinding.loading.setOnTouchListener((v, event) -> true);
+
+        activityRuleManagementBinding.searchButton.setOnClickListener(v -> {
+            if(searchTask!=null)
+            {
+                searchTask.cancel(true);
+            }
+            searchTask = new SearchTask();
+            searchTask.execute();
+        });
+
+        activityRuleManagementBinding.normalButton.setOnClickListener(v -> {
+            if(updateTask!=null)
+            {
+                updateTask.cancel(true);
+            }
+            updateTask = new UpdateTask(UpdateUserType.active, !ruleManagementViewModel.isActive());
+            updateTask.execute();
+        });
+
+        activityRuleManagementBinding.staffButton.setOnClickListener(v -> {
+            if(updateTask!=null)
+            {
+                updateTask.cancel(true);
+            }
+            updateTask = new UpdateTask(UpdateUserType.staff, !ruleManagementViewModel.isStaff());
+            updateTask.execute();
+        });
+
+        activityRuleManagementBinding.adminButton.setOnClickListener(v -> {
+            if(updateTask!=null)
+            {
+                updateTask.cancel(true);
+            }
+            updateTask = new UpdateTask(UpdateUserType.admin, !ruleManagementViewModel.isAdmin());
+            updateTask.execute();
+        });
+    }
+
+    private final class SearchTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            ruleManagementViewModel.setLoading(true);
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+            UserRepository.getInstance().getUserByEmailFromFireStore(ruleManagementViewModel.getEmail(), user -> {
+                if(user != null)
+                {
+                    Log.e(TAG, "find user success.");
+                    selectedUser = user;
+                    ruleManagementViewModel.setName(user.getName());
+                    ruleManagementViewModel.setEmail(user.getEmail());
+
+                    ruleManagementViewModel.setActive(user.isActive());
+                    ruleManagementViewModel.setStaff(user.isStaff());
+                    ruleManagementViewModel.setAdmin(user.isAdmin());
+
+                    Glide.with(getApplicationContext())
+                            .load(user.getAvatarUrl())
+                            .into(activityRuleManagementBinding.imageView);
+
+                    ruleManagementViewModel.setCanFind(true);
+                    ruleManagementViewModel.setLoading(false);
+                }
+                else
+                {
+                    Log.e(TAG, "find user failed.");
+                    selectedUser = null;
+                    ruleManagementViewModel.setCanFind(false);
+                    ruleManagementViewModel.setLoading(false);
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+        }
+    }
+
+    private final class UpdateTask extends AsyncTask<Void, Void, Void> {
+        private final UpdateUserType type;
+        private final boolean value;
+        public UpdateTask(UpdateUserType type, boolean value)
+        {
+            this.type = type;
+            this.value = value;
+        }
+        @Override
+        protected void onPreExecute() {
+            ruleManagementViewModel.setLoading(true);
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+            if(type == UpdateUserType.active)
+            {
+                UserRepository.getInstance().updateUserActiveAccess(selectedUser.getId(), value, success -> {
+                    if(success)
+                    {
+                        Log.e(TAG, "set user's active access success.");
+                        ruleManagementViewModel.setActive(value);
+                        ruleManagementViewModel.setLoading(false);
+                        Toast.makeText(
+                                RuleManagementActivity.this,
+                                "Đã chỉnh quyền thành công.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                    else
+                    {
+                        Log.e(TAG, "set user's active access failed.");
+                        ruleManagementViewModel.setLoading(false);
+                        Toast.makeText(
+                                RuleManagementActivity.this,
+                                "Đã có lỗi xãy ra, xin hãy thử lại sau.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                });
+            }
+            else if(type == UpdateUserType.staff)
+            {
+                UserRepository.getInstance().updateUserStaffAccess(selectedUser.getId(), value, success -> {
+                    if(success)
+                    {
+                        Log.e(TAG, "set user's staff access success.");
+                        ruleManagementViewModel.setStaff(value);
+                        ruleManagementViewModel.setLoading(false);
+                        Toast.makeText(
+                                RuleManagementActivity.this,
+                                "Đã chỉnh quyền thành công.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                    else
+                    {
+                        Log.e(TAG, "set user's staff access failed.");
+                        ruleManagementViewModel.setLoading(false);
+                        Toast.makeText(
+                                RuleManagementActivity.this,
+                                "Đã có lỗi xãy ra, xin hãy thử lại sau.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                });
+            }
+            else
+            {
+                UserRepository.getInstance().updateUserAdminAccess(selectedUser.getId(), value, success -> {
+                    if(success)
+                    {
+                        Log.e(TAG, "set user's admin access success.");
+                        ruleManagementViewModel.setAdmin(value);
+                        ruleManagementViewModel.setLoading(false);
+                        Toast.makeText(
+                                RuleManagementActivity.this,
+                                "Đã chỉnh quyền thành công.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                    else
+                    {
+                        Log.e(TAG, "set user's admin access failed.");
+                        ruleManagementViewModel.setLoading(false);
+                        Toast.makeText(
+                                RuleManagementActivity.this,
+                                "Đã có lỗi xãy ra, xin hãy thử lại sau.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+        }
+    }
+
+    private enum UpdateUserType{
+        active,
+        staff,
+        admin
+    }
+}
---
+++ coffee_shop_staff_admin/app/src/main/java/com/example/coffee_shop_staff_admin/viewmodels/RuleManagementViewModel.java
@@ -0,0 +1,155 @@
+package com.example.coffee_shop_staff_admin.viewmodels;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.coffee_shop_staff_admin.BR;
+
+public class RuleManagementViewModel extends BaseObservable {
+    @Bindable
+    private boolean loading = false;
+
+    public boolean isLoading() {
+        return loading;
+    }
+
+    public void setLoading(boolean loading) {
+        this.loading = loading;
+        notifyPropertyChanged(BR.loading);
+    }
+
+    @Bindable
+    private boolean canFind = false;
+
+    public boolean isCanFind() {
+        return canFind;
+    }
+
+    public void setCanFind(boolean canFind) {
+        this.canFind = canFind;
+        notifyPropertyChanged(BR.canFind);
+    }
+
+    @Bindable
+    private String email = "";
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+
+    public void onEmailChanged(CharSequence s, int start, int before, int count)
+    {
+        email = s.toString();
+    }
+
+    @Bindable
+    private String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+    @Bindable
+    private boolean active = true;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+        notifyPropertyChanged(BR.active);
+        if(active)
+        {
+            setNormalUserText("Chặn");
+        }
+        else
+        {
+            setNormalUserText("Bỏ chặn");
+        }
+    }
+
+    @Bindable
+    private String normalUserText = "Chặn";
+
+    public String getNormalUserText() {
+        return normalUserText;
+    }
+
+    public void setNormalUserText(String normalUserText) {
+        this.normalUserText = normalUserText;
+        notifyPropertyChanged(BR.normalUserText);
+    }
+
+    @Bindable
+    private boolean staff = true;
+
+    public boolean isStaff() {
+        return staff;
+    }
+
+    public void setStaff(boolean staff) {
+        this.staff = staff;
+        notifyPropertyChanged(BR.staff);
+        if(staff)
+        {
+            setStaffText("Bỏ quyền");
+        }
+        else
+        {
+            setStaffText("Cài quyền");
+        }
+    }
+    @Bindable
+    private String staffText = "Cài quyền";
+
+    public String getStaffText() {
+        return staffText;
+    }
+
+    public void setStaffText(String staffText) {
+        this.staffText = staffText;
+        notifyPropertyChanged(BR.staffText);
+    }
+
+
+    @Bindable
+    private boolean admin = true;
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+        notifyPropertyChanged(BR.admin);
+        if(admin)
+        {
+            setAdminText("Bỏ quyền");
+        }
+        else
+        {
+            setAdminText("Cài quyền");
+        }
+    }
+    @Bindable
+    private String adminText = "Cài quyền";
+
+    public String getAdminText() {
+        return adminText;
+    }
+
+    public void setAdminText(String adminText) {
+        this.adminText = adminText;
+        notifyPropertyChanged(BR.adminText);
+    }
+}
