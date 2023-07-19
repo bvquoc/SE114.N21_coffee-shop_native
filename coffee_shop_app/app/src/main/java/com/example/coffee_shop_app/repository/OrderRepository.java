436dcfab5e343475c25dc80446c8d8432eac07d7
kimthu09 <21521495@gm.uit.edu.vn>
2023-07-19 21:20:56 +0700

    fix: order list sort

--- coffee_shop_app/app/src/main/java/com/example/coffee_shop_app/repository/OrderRepository.java
+++ coffee_shop_app/app/src/main/java/com/example/coffee_shop_app/repository/OrderRepository.java
@@ -26,9 +26,13 @@ import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.joda.time.DateTime;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -74,6 +78,11 @@ public class OrderRepository {
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                 Log.d(TAG, "get order started.");
                 List<Order> listOrder=new ArrayList<>();
+                if(value==null || value.isEmpty()){
+                    orderListMutableLiveData.postValue(listOrder);
+                    Log.d(TAG, "no orders");
+                    return;
+                }
                 for (QueryDocumentSnapshot doc :
                         value) {
                     Map<String, Object> map = doc.getData();
@@ -144,6 +153,20 @@ public class OrderRepository {
                                                         currentOrder.setProducts(listOrderFood);
                                                     }
                                                     listOrder.add(currentOrder);
+                                                    Collections.sort(listOrder, new Comparator<Order>() {
+                                                        @Override
+                                                        public int compare(Order o1, Order o2) {
+                                                            DateTime d1=new DateTime(o1.getDateOrder());
+                                                            DateTime d2=new DateTime(o2.getDateOrder());
+                                                            if(d1.isEqual(d2)){
+                                                                return 0;
+                                                            } else if(d1.isAfter(d2)){
+                                                                return -1;
+                                                            }else{
+                                                                return 1;
+                                                            }
+                                                        }
+                                                    });
                                                     orderListMutableLiveData.postValue(listOrder);
                                                 } else{
                                                     Log.e(TAG, "get order food failed.");
